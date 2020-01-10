59b22598532b8a87c95fbd1092e09a6a2760ba16
Grigory <ovonely@gmail.com>
2020-01-10 18:50:23 +0700

    Spring Rest - json data binding:   - ignoring unknown properties

--- src/main/java/com/nga/jackson/json/demo/Student.java
+++ src/main/java/com/nga/jackson/json/demo/Student.java
@@ -1,5 +1,8 @@
 package com.nga.jackson.json.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
 
     private int id;
