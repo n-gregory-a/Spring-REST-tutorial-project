310d9460b681ba2166d84595169b58364fbe32b6
Grigory <ovonely@gmail.com>
2020-01-10 17:06:57 +0700

    Spring Rest - json data binding:   - moving sample-lite.json and sample-full.json to the RIGHT place   - creating Student class   - creating Driver class for reading simple json file

---
+++ src/main/java/com/nga/jackson/json/demo/Driver.java
@@ -0,0 +1,29 @@
+package com.nga.jackson.json.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        try {
+
+            // create object mapper
+            ObjectMapper mapper = new ObjectMapper();
+
+            // read JSON file and map/convert to java POJO
+            // data/sample-lite.json
+
+            Student student = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+
+            // print first name and last name
+            System.out.println("First name = " + student.getFirstName());
+            System.out.println("Last name = " + student.getLastName());
+
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+}
