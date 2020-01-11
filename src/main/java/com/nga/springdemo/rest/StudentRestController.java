ae4b63ba12d3e795d3015f6df2b95e5b5d4a3689
Grigory <ovonely@gmail.com>
2020-01-11 16:47:04 +0700

    Spring Rest - retrieve POJO as JSON:   - using @PathVariable to retrieve single student

--- src/main/java/com/nga/springdemo/rest/StudentRestController.java
+++ src/main/java/com/nga/springdemo/rest/StudentRestController.java
@@ -2,9 +2,11 @@ package com.nga.springdemo.rest;
 
 import com.nga.springdemo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,17 +14,32 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> students;
 
-        List<Student> students = new ArrayList<>();
+    // define @PostConstruct to load the student data ... only once!
+    @PostConstruct
+    public void loadData() {
+
+        students = new ArrayList<>();
 
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Mary", "Smith"));
+    }
+
+    // define endpoint for "/students" - return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return students;
+    }
+
+    // define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list ... keep it simple for now
+        return students.get(studentId);
     }
 
 }
